638f8350089b6ef5b69708f6b356633793229e88
yaypixxo <viggo@lekdorf.com>
2020-01-03 12:37:44 +0100

    twitter button

--- kageprefs/KgeRootListController.m
+++ kageprefs/KgeRootListController.m
@@ -10,6 +10,28 @@
 	return _specifiers;
 }
 
+-(void)openTwitter {
+	NSURL *url;
+
+	if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot:"]]) {
+		url = [NSURL URLWithString:@"tweetbot:///user_profile/Ra1nPix"];
+	}
+	else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitterrific:"]]) {
+		url = [NSURL URLWithString:@"twitterrific:///profile?screen_name=Ra1nPix"];
+	}
+	else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetings:"]]) {
+		url = [NSURL URLWithString:@"tweetings:///user?screen_name=Ra1nPix"];
+	}
+	else if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter:"]]) {
+		url = [NSURL URLWithString:@"twitter://user?screen_name=Ra1nPix"];
+	}
+	else {
+		url = [NSURL URLWithString:@"https://mobile.twitter.com/Ra1nPix"];
+	}
+
+	[[UIApplication sharedApplication] openURL:url];
+}
+
 -(void)saveTapped {
 	CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.yaypixxo.kage/respring"), NULL, NULL, YES);
 }
